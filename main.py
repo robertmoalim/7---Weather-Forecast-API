4c38164b77997f78798d40fb5d4d9fa22a72c9ba
robertmoalim <Robertbusiness0902@Hotmail.com>
2023-11-20 02:23:29 +0100

    Converted Kelvin to Celsius / Handled Errors for empty/wrong inputs

--- main.py
+++ main.py
@@ -11,9 +11,24 @@ option = st.selectbox("select data to view",
                       ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-# Get the temperature/sky data
-d, t = get_data(place, days, option)
+if place:
+    try:
+        # Get the temperature/sky data
+        filtered_data = get_data(place, days)
 
-# Create a temperature plot
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
-st.plotly_chart(figure)
+        if option == "Temperature":
+            temperatures = [dict["main"]["temp"] - 273.15 for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            # Create a temperature plot
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
+
+        if option == "Sky":
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                      "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            sky_conditions = filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
+            image_paths = [images[condition] for condition in sky_conditions]
+            st.image(image_paths, width=115)
+
+    except KeyError:
+        st.write("That country does not exist!")
